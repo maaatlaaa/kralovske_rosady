dfd994e8514d9413c40c921ab3ab92ed0ac36f1b
Matouš Nehasil <nehasmat@fit.cvut.cz>
2023-10-13 11:06:00 +0200

    kralovske_rosady: pridani třídy Karta

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Modul main.py představuje zakladní řídící jednotku pro naši hru.
+"""
+
+class Karta:
+    """ class Karta: obejkt predstavujici hraci kartu,
+    které postupně hráči vykládají na hrací plochu """
+    def __init__(self, nazev, popis, hodnota):
+        self.hodnota = hodnota
+        self.nazev = nazev
+        self.popis = popis
+
+    def get_nazev(self):
+        """ Vraci nazev sama sebe """
+        return self.nazev
+
+    def get_hodnota(self):
+        """ Vraci svou hodnotu """
+        return self.hodnota
